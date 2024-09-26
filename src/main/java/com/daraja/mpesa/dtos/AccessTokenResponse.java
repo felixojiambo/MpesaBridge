d1e6397dd8174deab215dcdd0696c17fdf103a33
felixojiambo <felixojiamboe@gmail.com>
2024-09-26 15:58:00 +0300

    try catch

--- src/main/java/com/daraja/mpesa/dtos/AccessTokenResponse.java
+++ src/main/java/com/daraja/mpesa/dtos/AccessTokenResponse.java
@@ -1,10 +1,24 @@
 package com.daraja.mpesa.dtos;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
+import lombok.SneakyThrows;
+
 @Data
 public class AccessTokenResponse {
     @JsonProperty("access_token")
     private String accessToken;
     @JsonProperty("expires_in")
     private String expiresIn;
+    @SneakyThrows
+    @Override
+    public String toString() {
+        try {
+            return new ObjectMapper().writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
