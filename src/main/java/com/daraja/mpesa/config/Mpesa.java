363f47c42b784b9a503a9840ac3797e7d27db02c
felixojiambo <felixojiamboe@gmail.com>
2024-09-26 15:55:21 +0300

    mpesa key consumer

--- src/main/java/com/daraja/mpesa/config/Mpesa.java
+++ src/main/java/com/daraja/mpesa/config/Mpesa.java
@@ -1,7 +1,16 @@
 package com.daraja.mpesa.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "mpesa.daraja")
 public class Mpesa {
-    private String appKey;
-    private String appSecret;
+
+    private String consumerKey;
+    private String consumerSecret;
+    private String grantType;
+    private String oauthEndpoint;
 }
