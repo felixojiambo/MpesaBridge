556a38a7adf2b87ca655bad3cc3d8a4e8bf8e90e
felixojiambo <felixojiamboe@gmail.com>
2024-09-26 21:06:56 +0300

    Add registerURL method in DarajaApi for registering URLs with M-Pesa

--- src/main/java/com/daraja/mpesa/services/DarajaApi.java
+++ src/main/java/com/daraja/mpesa/services/DarajaApi.java
@@ -1,5 +1,9 @@
 package com.daraja.mpesa.services;
 import com.daraja.mpesa.dtos.AccessTokenResponse;
+import com.daraja.mpesa.dtos.RegisterURLResponse;
+
 public interface DarajaApi {
     AccessTokenResponse getAccessToken();
+
+    RegisterURLResponse registerURL();
 }
