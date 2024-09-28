f20cc7a31d16ac7090075b1da8047cd8a3ad2ae5
felixojiambo <felixojiamboe@gmail.com>
2024-09-28 15:11:04 +0300

    feat: Implement B2C Transaction with Access Token, Security Credentials, and Logging
    - Implemented the `performB2CTransaction` method to execute B2C transactions using access token.
    - Mapped internal request to external B2CTransactionRequest.
    - Integrated security credentials using `HelperUtility.getSecurityCredentials`.
    - Added parameterized logging for sensitive fields like security credentials and access token.
    - Utilized OkHttpClient to make API request to B2C endpoint.
    - Exception handling with improved logging for error tracing.

--- src/main/java/com/daraja/mpesa/services/DarajaApi.java
+++ src/main/java/com/daraja/mpesa/services/DarajaApi.java
@@ -1,12 +1,10 @@
 package com.daraja.mpesa.services;
-import com.daraja.mpesa.dtos.AccessTokenResponse;
-import com.daraja.mpesa.dtos.RegisterURLResponse;
-import com.daraja.mpesa.dtos.SimulateTransactionRequest;
-import com.daraja.mpesa.dtos.SimulateTransactionResponse;
+import com.daraja.mpesa.dtos.*;
 
 public interface DarajaApi {
     AccessTokenResponse getAccessToken();
 
     RegisterURLResponse registerURL();
     SimulateTransactionResponse simulateC2BTransaction(SimulateTransactionRequest simulateTransactionRequest);
+    B2CTransactionSyncResponse performB2CTransaction(InternalB2CTransactionRequest internalB2CTransactionRequest) throws Exception;
 }
