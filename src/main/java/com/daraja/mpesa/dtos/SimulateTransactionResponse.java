6de5b407128f0b0ec3ca2899dae6ec652eef891f
felixojiambo <felixojiamboe@gmail.com>
2024-09-28 10:14:52 +0300

    c2b dtos

---
+++ src/main/java/com/daraja/mpesa/dtos/SimulateTransactionResponse.java
@@ -0,0 +1,16 @@
+package com.daraja.mpesa.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class SimulateTransactionResponse {
+    @JsonProperty("ConversationID")
+    private String conversationID;
+
+    @JsonProperty("ResponseDescription")
+    private String responseDescription;
+
+    @JsonProperty("OriginatorCoversationID")
+    private String originatorCoversationID;
+}
